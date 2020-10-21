6de841f00cd5afe5bacc6fced979ecf78ab77251
GreenLadyTea <kudinovamo@mer.ci.nsu.ru>
2020-10-21 18:25:18 +0700

    different configs
    configs for development and production

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -4,14 +4,12 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const paths = {
-    src: path.resolve(__dirname, './src'), // source files
-    build: path.resolve(__dirname, './build'), // production build files
-    static: path.resolve(__dirname, './public'), // static files to copy to build folder
+    src: path.resolve(__dirname, '../src'), // source files
+    build: path.resolve(__dirname, '../build'), // production build files
+    static: path.resolve(__dirname, '../public'), // static files to copy to build folder
 };
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
     entry: [paths.src + '/index.js'],
     output: {
         path: paths.build,
@@ -26,8 +24,6 @@ module.exports = {
             ],
         }),
         new HtmlWebpackPlugin({
-            //title: 'Super chat',
-            //favicon: paths.static + '/favicon.png',
             template: paths.static + '/index.html', // template file
             filename: 'index.html', // output file
         }),
