d67ea7bb622087d317ac80e72a1a54764b68d24e
GreenLadyTea <kudinovamo@mer.ci.nsu.ru>
2020-11-19 18:06:37 +0700

    registration form with user creation

--- configs/webpack.development.js
+++ configs/webpack.development.js
@@ -1,19 +1,18 @@
-const { merge } = require('webpack-merge');
-const webpack = require('webpack');
-const common = require('./webpack.common.js');
-const paths = require('./paths');
+const { merge } = require("webpack-merge");
+const webpack = require("webpack");
+const common = require("./webpack.common.js");
+const paths = require("./paths");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        historyApiFallback: true,
-        contentBase: paths.build,
-        open: true,
-        hot: true,
-    },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    historyApiFallback: true,
+    contentBase: paths.build,
+    open: true,
+    hot: true,
+    port: 3000
+  },
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-    ],
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 });
--- src/views/RegistrationView.js
+++ src/views/RegistrationView.js
@@ -1,10 +1,53 @@
 import React from "react";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3001"
+});
 
 export default class RegistrationView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nickname: "",
+      password: ""
+    };
+  }
+
+  handleSubmit(event) {
+    const { nickname, password } = this.state;
+    axiosInstance.post("/user", { nickname, password });
+    event.preventDefault();
+  }
+
   render() {
+    const { nickname, password } = this.state;
     return (
       <>
         <h1>Регистрация</h1>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <div>
+            <label>
+              Никнейм:
+              <input
+                type="text"
+                value={nickname}
+                onChange={(e) => this.setState({ nickname: e.target.value })}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Пароль:
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => this.setState({ password: e.target.value })}
+              />
+            </label>
+          </div>
+          <button type="submit">Создать пользователя</button>
+        </form>
       </>
     );
   }
